0304bd4382b41e77779c80e2085625ba31ec11d3
Joe Blankenship <thejoeblankenship@protonmail.com>
2018-12-17 21:50:21 -0500

    update to ukpd website dashboard - simplification

--- ukpd_crimelog/js/app.js
+++ ukpd_crimelog/js/app.js
@@ -16,4 +16,45 @@
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
+    drawLegend();
+    drawSlider();
+
+    function drawLegend() {
+        // generate legend via leaflet
+        var legendControl = L.control({
+            position: 'topright'
+        });
+        // add to Map
+        legendControl.onAdd = function(map) {
+            // create new DOM element for id=legend
+            var legend = L.DomUtil.create('div', 'legend');
+            return legend;
+        };
+        // add legent element to map
+        legendControl.addTo(map);
+
+        // add legend title
+        var legend = $('.legend').html('<h3><span>2018</span> Crime Types</h3><ul>');
+
+    };
+
+    function drawSlider() {
+        // create Leaflet control for slider
+        var sliderControl = L.control({
+            position: 'bottomleft'
+        });
+        // define the ui-control within the DOM
+        sliderControl.onAdd = function(map) {
+            // use the defined ui-control element
+            var slider = L.DomUtil.get("ui-controls");
+            // disable scrolling
+            L.DomEvent.disableScrollPropagation(slider);
+            // diable click events
+            L.DomEvent.disableClickPropagation(slider);
+            return slider;
+        };
+        // add control to map
+        sliderControl.addTo(map);
+
+    };
 })();
