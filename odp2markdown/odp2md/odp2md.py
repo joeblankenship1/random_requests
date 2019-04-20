fcb4b9a7ef7d8e96471ea2355ab0064257f32361
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-04-19 23:03:32 -0400

    odp2md - added odp2md lib file

---
+++ odp2markdown/odp2md/odp2md.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+from pdfx import PDFx
+
+data_dir = Path('.')
+
+def odp_to_pdf(filename):
+    odp_file = data_dir / filename
+    os.system(f'libreoffice --headless --convert-to pdf {odp_file}')
+
+def pdf_to_text(filename):
+    pdf_file = data_dir / filename
+    pdf_data = PDFx(pdf_file)
+    pdf_text = pdf_data.get_text()
+    return pdf_text
+
+def text_to_md(filename):
+    pass
