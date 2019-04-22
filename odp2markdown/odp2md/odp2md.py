d6341ce1229ac56c98a08a9231dafb68bd86ebbb
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-04-21 21:20:53 -0400

    odp2md - update odp2md file

--- odp2markdown/odp2md/odp2md.py
+++ odp2markdown/odp2md/odp2md.py
@@ -9,10 +9,11 @@ def odp_to_pdf(filename):
     os.system(f'libreoffice --headless --convert-to pdf {odp_file}')
 
 def pdf_to_text(filename):
-    pdf_file = data_dir / filename
+    pdf_file = str(data_dir / filename)
     pdf_data = PDFx(pdf_file)
     pdf_text = pdf_data.get_text()
     return pdf_text
 
-def text_to_md(filename):
-    pass
+def text_to_md(text):
+    text_file = text
+    
