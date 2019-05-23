d7f8f82ed49f6259fea0873ddc9b71ae3d08b003
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-05-22 21:10:20 -0400

    odp2md - added docstring to functions

--- odp2markdown/odp2md/odp2md.py
+++ odp2markdown/odp2md/odp2md.py
@@ -5,12 +5,22 @@ from pdfx import PDFx
 data_dir = Path('.')
 
 def odp_to_pdf(filename):
+    """
+    This function converts ODP files to PDF
+    """
     odp_file = data_dir / filename
     os.system(f'libreoffice --headless --convert-to pdf {odp_file}')
 
 def pdf_to_text(filename):
+    """
+    This function converts pdf text to markdown file.
+    This file still needs to be cleaned up.
+    """
     pdf_file = str(data_dir / filename)
     pdf_data = PDFx(pdf_file)
     pdf_text = pdf_data.get_text()
     with open(f'{filename}.md', 'w') as f:
         f.write(pdf_text)
+
+def clean_text(filename):
+    pass
