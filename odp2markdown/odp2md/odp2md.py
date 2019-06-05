e8263f19cfb0209aef98d8343bec1f4c177f6052
Joe Blankenship <thejoeblankenship@protonmail.com>
2019-06-04 22:04:40 -0400

    odp2md - added image extractor function

--- odp2markdown/odp2md/odp2md.py
+++ odp2markdown/odp2md/odp2md.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from pathlib import Path
 from pdfx import PDFx
 
@@ -23,4 +24,22 @@ def pdf_to_text(filename):
         f.write(pdf_text)
 
 def clean_text(filename):
+    """
+    This function cleans up bullets and other markings
+    from the ODP format that transferred into markdown
+    """
     pass
+
+def extract_images(filename, output_dir):
+    """
+    This function extracts images from pdfs
+    Thanks to Mike Driscoll for the code :-)
+    """
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+ 
+    cmd = ['pdfimages', '-all', filename, 
+           '{}/prefix'.format(output_dir)]
+    subprocess.call(cmd)
+    print('Images extracted:')
+    print(os.listdir(output_dir))
