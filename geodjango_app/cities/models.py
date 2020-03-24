e13194e9a145b7aacc118c2ce8eb5b2efe87893a
Joe Blankenship <thejoeblankenship@protonmail.com>
2020-03-23 23:56:34 -0400

    geodjango - modify city model

--- geodjango_app/cities/models.py
+++ geodjango_app/cities/models.py
@@ -2,28 +2,10 @@ from django.db import models
 from django.contrib.gis.db import models as geomodels
 
 
-class Cities500(models.Model):
-    geom = models.PointField(blank=True, null=True)
-    field_1 = models.IntegerField(blank=True, null=True)
-    field_2 = models.CharField(max_length=-1, blank=True, null=True)
-    field_3 = models.CharField(max_length=-1, blank=True, null=True)
-    field_4 = models.CharField(max_length=-1, blank=True, null=True)
-    field_5 = models.FloatField(blank=True, null=True)
-    field_6 = models.FloatField(blank=True, null=True)
-    field_7 = models.CharField(max_length=-1, blank=True, null=True)
-    field_8 = models.CharField(max_length=-1, blank=True, null=True)
-    field_9 = models.CharField(max_length=-1, blank=True, null=True)
-    field_10 = models.CharField(max_length=-1, blank=True, null=True)
-    field_11 = models.CharField(max_length=-1, blank=True, null=True)
-    field_12 = models.CharField(max_length=-1, blank=True, null=True)
-    field_13 = models.CharField(max_length=-1, blank=True, null=True)
-    field_14 = models.CharField(max_length=-1, blank=True, null=True)
-    field_15 = models.IntegerField(blank=True, null=True)
-    field_16 = models.IntegerField(blank=True, null=True)
-    field_17 = models.IntegerField(blank=True, null=True)
-    field_18 = models.CharField(max_length=-1, blank=True, null=True)
-    field_19 = models.CharField(max_length=-1, blank=True, null=True)
+class City(models.Model):
+    geom = geomodels.PointField()
+    name = models.CharField(max_length=100, blank=False)
 
     class Meta:
-        managed = True
-        db_table = 'cities500'
+        ordering = ('name',)
+        verbose_name_plural = 'cities'
