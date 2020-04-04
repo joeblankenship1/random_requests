84524d89b4c78891d9a6400a297f4d453066f07d
Joe Blankenship <thejoeblankenship@protonmail.com>
2020-04-03 23:30:43 -0400

    sql2csv - update lib funct

--- sql2csv/sql2csv/sql2csv.py
+++ sql2csv/sql2csv/sql2csv.py
@@ -1,6 +1,16 @@
-query = open('/tmp/enwiki-latest-category.sql', 'rb')
+#! python3
 
-with open('/tmp/test.txt', 'w') as file:
-    for line in query.readlines():
-        file.write(str(line))
 
+# %%
+def get_sql_data(file_name):
+    """"""
+    sql_data = open(file_name, 'rb')
+    return sql_data
+
+
+# %%
+def data_to_text(data, output):
+    """"""
+    with open(output, 'w') as file:
+        for line in data.readlines():
+            file.write(str(line))
